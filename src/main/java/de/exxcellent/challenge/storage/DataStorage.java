ec509d3a50e7ddd6658b16d475891b95921119fe
Evelyn <bewerbung@maephes.com>
2021-11-24 13:50:36 +0100

    Add structure for data storage class to save parsed data

---
+++ src/main/java/de/exxcellent/challenge/storage/DataStorage.java
@@ -0,0 +1,15 @@
+package de.exxcellent.challenge.storage;
+
+import de.exxcellent.challenge.data.Weather;
+
+import java.util.List;
+
+public class DataStorage {
+
+    private List<Weather> storage;
+
+
+    public static List<Weather> parseStringToWeatherList(List<List<String>> csvData) {
+        
+    }
+}
--- src/test/java/de/exxcellent/challenge/CSVParserTest.java
+++ src/test/java/de/exxcellent/challenge/CSVParserTest.java
@@ -1,8 +1,9 @@
 package de.exxcellent.challenge;
 
 import de.exxcellent.challenge.constants.FilePath;
+import de.exxcellent.challenge.data.Weather;
 import de.exxcellent.challenge.parser.CSVParser;
-import org.junit.jupiter.api.DisplayName;
+import de.exxcellent.challenge.storage.DataStorage;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,16 +16,33 @@ public class CSVParserTest {
     private CSVParser csvParser = new CSVParser();
 
     @Test
-    @DisplayName("Read content from csv file to List<List<String>>")
+    //@DisplayName("Read content from csv file to List<List<String>>")
     void readCSVFile() {
         List<List<String>> csvData = csvParser.readFile(FilePath.testCSVFilePath);
         assertTrue(csvData.size() > 0);
     }
 
     @Test
-    @DisplayName("Test if all lines are added to the list")
+    //@DisplayName("Test if all lines are added to the list")
     void getAllLinesFromCSVFile() {
         List<List<String>> csvData = csvParser.readFile(FilePath.testCSVFilePath);
         assertEquals(4, csvData.size());
+    }
+
+    @Test
+    //@DisplayName("Test if all lines from weather csv are added to the list")
+    void getAllLinesFromWeatherCSVFile() {
+        List<List<String>> csvData = csvParser.readFile(FilePath.weatherFilePath);
+        assertEquals(31, csvData.size());
+    }
+
+    @Test
+    //@DisplayName()
+    void parseListToWeatherObjects() {
+        List<List<String>> csvData = csvParser.readFile(FilePath.weatherFilePath);
+        List<Weather> weatherData = DataStorage.parseStringToWeatherList(csvData);
+        assertEquals(weatherData.get(1).getDay(), 1);
+        assertEquals(weatherData.get(1).getMaximumTemperature(), 88);
+        assertEquals(weatherData.get(1).getMinimumTemperature(), 59);
     }
 }
